b57ddbea0e0577196bdbea6d7cd4fde44411ad9f
Sam <samdup123@gmail.com>
2018-02-04 20:01:38 -0500

    tests passing

--- src/Fsm/Fsm.lua
+++ src/Fsm/Fsm.lua
@@ -1,5 +1,19 @@
+
+local currentState
+
 return {
   init = function(initialState)
-    initialState()
+    currentState = initialState
+    currentState('entry')
+    return {
+      transitionTo = function(newState)
+        currentState('exit')
+        currentState = newState
+        currentState('entry')
+      end,
+      sendSignal = function(signal)
+        currentState(signal)
+      end
+    }
   end
 }
