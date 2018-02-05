1f194e64e98a8db03584561909393086acccebb8
Sam <samdup123@gmail.com>
2018-02-04 20:07:05 -0500

    cleanup

--- src/Fsm/Fsm.lua
+++ src/Fsm/Fsm.lua
@@ -1,19 +1,19 @@
-
 local currentState
 
-return {
-  init = function(initialState)
-    currentState = initialState
-    currentState('entry')
-    return {
-      transitionTo = function(newState)
-        currentState('exit')
-        currentState = newState
-        currentState('entry')
-      end,
-      sendSignal = function(signal)
-        currentState(signal)
-      end
-    }
-  end
-}
+local transitionTo = function(newState)
+  currentState('exit')
+  currentState = newState
+  currentState('entry')
+end
+
+local sendSignal = function(signal)
+  currentState(signal)
+end
+
+local init = function(initialState)
+  currentState = initialState
+  currentState('entry')
+  return {transitionTo = transitionTo, sendSignal = sendSignal}
+end
+
+return {init = init}
